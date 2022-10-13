4ec33c746621004ad60a5391960663d895e4be27
AJIyanu <aderemijoseph@live.com>
2022-10-13 06:45:58 +0100

    no privelege but select

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -5,5 +5,5 @@ CREATE SCHEMA IF NOT EXISTS hbnb_test_db;
 CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
-GRANT SELECT PRIVILEGES ON performance_schema.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
