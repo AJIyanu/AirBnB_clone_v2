3c549c444d3e413d7b58f8864a0c57fbcd351363
AJIyanu <aderemijoseph@live.com>
2022-10-13 06:43:05 +0100

    permissions

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,7 +3,7 @@
 
 CREATE SCHEMA IF NOT EXISTS hbnb_dev_db;
 CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON 'hbnb_dev_db' . * TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
-GRANT SELECT PRIVILEGES ON 'performance_schema' . * TO 'hbnb_dev'@'localhost';
+GRANT SELECT PRIVILEGES ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
