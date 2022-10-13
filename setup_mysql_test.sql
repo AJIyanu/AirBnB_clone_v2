4b4541a19bbd60f29cf10bcf001da933c14f3aed
AJIyanu <aderemijoseph@live.com>
2022-10-13 06:40:38 +0100

    test db

---
+++ setup_mysql_test.sql
@@ -0,0 +1,9 @@
+-- This script prepares my clone for database
+-- it creates database and users
+
+CREATE SCHEMA IF NOT EXISTS hbnb_test_db;
+CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+GRANT SELECT PRIVILEGES ON performance_schema . * TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
