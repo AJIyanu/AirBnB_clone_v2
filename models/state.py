f85eec90a9df7a12154dd791cd0d99b50adc6074
AJIyanu <aderemijoseph@live.com>
2022-10-19 06:48:40 +0100

    to be dead

--- models/state.py
+++ models/state.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from sqlalchemy import Table, Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
 from models.base_model import Base, BaseModel
 
 
@@ -8,3 +10,4 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
+    city_rel = relationship("City")
