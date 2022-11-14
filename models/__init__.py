eb99f5889b155c73fe5602b77727ed1ede432f77
AJIyanu <aderemijoseph@live.com>
2022-11-14 13:12:45 +0100

    done  with storage type now

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 
-if HBNB_TYPE_STORAGE is "db":
+
+import os
+
+strgtype = os.getenv("HBNB_TYPE_STORAGE")
+if strgtype is "db":
     from models.engine.file_storage import FileStorage as Storage
 else:
     from models.engine.db_storage import DBStorage as Storage
@@ -9,5 +13,3 @@ else:
 
 storage = Storage()
 storage.reload()
-
-
