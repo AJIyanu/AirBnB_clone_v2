3f5e8fe052a1a62ffe26dae0c424688db292c93e
AJIyanu <aderemijoseph@live.com>
2022-11-14 17:58:30 +0100

    right place

--- models/__init__.py
+++ models/__init__.py
@@ -6,11 +6,11 @@ import os
 
 strgtype = os.getenv("HBNB_TYPE_STORAGE")
 if strgtype == "db":
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage()
-else:
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 
 storage.reload()
 print(type(storage))
