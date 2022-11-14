bb515516fc3df8e0de1251a0f78dbc051568b921
AJIyanu <aderemijoseph@live.com>
2022-11-14 13:06:27 +0100

    import based on storage type

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+
+if HBNB_TYPE_STORAGE is "db":
+    from models.engine.file_storage import FileStorage as Storage
+else:
+    from models.engine.db_storage import DBStorage as Storage
 
 
-storage = FileStorage()
+storage = Storage()
 storage.reload()
+
+
