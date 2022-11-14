31075a08135e5061a7be185ded11616be91e2713
AJIyanu <aderemijoseph@live.com>
2022-11-14 17:44:16 +0100

    == not sea

--- models/__init__.py
+++ models/__init__.py
@@ -5,7 +5,7 @@
 import os
 
 strgtype = os.getenv("HBNB_TYPE_STORAGE")
-if strgtype is "db":
+if strgtype == "db":
     from models.engine.file_storage import FileStorage as Storage
 else:
     from models.engine.db_storage import DBStorage as Storage
