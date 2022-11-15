dbd4cf03106d1f0aeea316a6aff5f93596229920
AJIyanu <aderemijoseph@live.com>
2022-11-15 04:53:22 +0100

    update fews

--- models/city.py
+++ models/city.py
@@ -3,6 +3,7 @@
 from sqlalchemy import Table, Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from models.base_model import Base, BaseModel
+from  models.place import Place
 
 
 class City(BaseModel, Base):
@@ -10,3 +11,4 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
     name = Column(String(128), nullable=False)
+    places = relationship("Place", backref="cities", cascade="all, delete")
