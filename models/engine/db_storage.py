70c99ecfcb62b32c51babef397d75117e22c91a8
AJIyanu <aderemijoseph@live.com>
2022-11-15 03:06:51 +0100

    why not iterable

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -57,9 +57,9 @@ class DBStorage:
                 finally:
                     continue
         objdict = {}
-        for clsdb in result:
+        '''for clsdb in result:
             key = "{}.{}".format(clsdb.__name__, clsdb.id)
-            objdict.update({key: clsdb})
+            objdict.update({key: clsdb})'''
         return (result)
 
     def new(self, obj):
