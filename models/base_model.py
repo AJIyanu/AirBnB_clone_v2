ab86c339c53f0c36427544dd65cc3aa307b0b2a7
AJIyanu <aderemijoseph@live.com>
2022-11-16 21:36:10 +0100

    no dey gimme wahala

--- models/base_model.py
+++ models/base_model.py
@@ -20,12 +20,15 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+            try:
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+                del kwargs['__class__']
+                self.__dict__.update(kwargs)
+            except Exception:
+                pass
 
     def __str__(self):
         """Returns a string representation of the instance"""
