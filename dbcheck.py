df70e8bd2df402f87b2eb173eb143b5aed315bbe
AJIyanu <aderemijoseph@live.com>
2022-11-16 21:30:03 +0100

    checking error

--- dbcheck.py
+++ dbcheck.py
@@ -1,18 +1,36 @@
 #!/usr/bin/python3
-"""
-check my code
-"""
-
-from models.engine.db_storage import DBStorage
-from models.place import Place
+from models import *
 from models.state import State
 from models.city import City
+from models.user import User
+from models.place import Place
+from models.amenity import Amenity
 
-db = DBStorage()
+s = State(name="California")
+s.save()
+c = City(state_id=s.id, name="San Francisco")
+c.save()
 
-#trying to reload all datatable
-db.reload()
-alltable = db.all()
-for one in alltable:
-    print("{}, wetin dey be this {}".format(one, alltable[one]))
-#print("{}".format(alltable))
+u = User(email="a@a.com", password="pwd")
+u.save()
+
+p1 = Place(user_id=u.id, city_id=c.id, name="House 1")
+p1.save()
+p2 = Place(user_id=u.id, city_id=c.id, name="House 2")
+p2.save()
+
+a1 = Amenity(name="Wifi")
+a1.save()
+a2 = Amenity(name="Cable")
+a2.save()
+a3 = Amenity(name="Eth")
+a3.save()
+
+p1.amenities.append(a1)
+p1.amenities.append(a2)
+
+p2.amenities.append(a1)
+p2.amenities.append(a2)
+p2.amenities.append(a3)
+
+storage.save()
