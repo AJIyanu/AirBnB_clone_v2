e4ed3a3897a49d5fd7afdda772a267ae9d8645be
AJIyanu <aderemijoseph@live.com>
2022-11-16 21:44:58 +0100

    init

--- models/state.py
+++ models/state.py
@@ -14,6 +14,10 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="all, delete")
 
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
     @property
     def cities(self):
         """create a relationship for state in Filestorage"""
