b1f3bc09b89e654725576bffea319f3f487c2183
AJIyanu <aderemijoseph@live.com>
2022-11-27 03:54:57 +0100

    Python is cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+flask webframe work
+"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def index():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def index2():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def index3(text):
+    return "C {}".format(text.replace("_", " "))
+
+@app.route("/python/(<text>)")
+def display(text="is cool"):
+    return "Python {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
