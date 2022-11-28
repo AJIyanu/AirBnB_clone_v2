6b2ff47d6dd924c144e4c9d6533a1b2b011362b7
AJIyanu <aderemijoseph@live.com>
2022-11-28 16:18:56 +0100

    not done yet

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,47 @@
+
+#!/usr/bin/python3
+"""
+flask webframe work
+"""
+
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def index():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def index2():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def index3(text):
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python/", defaults={"text": "is cool"})
+@app.route("/python/<text>")
+def display(text):
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route("/number/<int:n>/")
+def integer_req(n):
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>")
+def serve_page(n):
+    return render_template("templates/5-number.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
