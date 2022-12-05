ee948311dd652943ebf9a757f94a568f15f9992b
AJIyanu <aderemijoseph@live.com>
2022-12-05 15:42:02 +0100

    user

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -15,7 +15,7 @@ from ..city import City
 #from ..place import Place
 #from ..review import Review
 from ..state import State
-#from ..user import User
+from ..user import User
 
 
 class DBStorage:
